3521af53b98e146e6cd171803d46412cb6d2da61
Andrea Coghi <andrea.coghi@biters.it>
2024-08-01 11:04:53 +0200

    test: added some junits

--- src/main/java/it/biters/demo/user/manager/data/entities/UserEntity.java
+++ src/main/java/it/biters/demo/user/manager/data/entities/UserEntity.java
@@ -1,19 +1,18 @@
 package it.biters.demo.user.manager.data.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "USERS")
 @Getter
 @Setter
 @ToString
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity implements Serializable {
 
     @Id
@@ -24,4 +23,23 @@ public class UserEntity implements Serializable {
     private String lastName;
     private String email;
     private String address;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(email, that.email) && Objects.equals(address, that.address);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(firstName);
+        result = 31 * result + Objects.hashCode(lastName);
+        result = 31 * result + Objects.hashCode(email);
+        result = 31 * result + Objects.hashCode(address);
+        return result;
+    }
 }
